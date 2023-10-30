dc6f0f461199cd8e8b685eb8ee4b44b37b5a6ea2
Lucas David Erkana <lderkana21x@gmail.com>
2023-10-30 10:46:00 +0200

    Clean

--- missingNumber.js
+++ missingNumber.js
@@ -1,8 +1,5 @@
 var missingNumber = function(nums) {
-    const n = nums.length;
-    const expectedSum = (n * (n + 1)) / 2; // Step 1
-    const actualSum = nums.reduce((sum, num) => sum + num, 0); // Step 2
-    return expectedSum - actualSum; // Step 3
+    //Write code here
 };
 
 console.log(missingNumber([3, 0, 1])); // expected result 2
